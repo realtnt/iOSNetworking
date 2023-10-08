172a3ac0ce954fa7ba0a8c68cd131a7642b66323
realtnt <theo@thetaphoto.co.uk>
2023-10-08 09:43:19 +0100

    Initial Commit

---
+++ Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by Theo Ntogiakos on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
